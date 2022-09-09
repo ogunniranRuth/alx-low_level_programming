afab7d3c9913bcc09e671c1091fc91dff5c5afec
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-09 04:49:15 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *main- serve as the entry point for the program*
  *Return: By default returns zero if no errors otherwise returns non-zero value.
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
