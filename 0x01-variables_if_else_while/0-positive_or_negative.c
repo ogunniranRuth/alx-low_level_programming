dbaf0f1d66017205439794b0a5ad2b415ed26797
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-09 08:23:13 -0700

    task commit 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *   * main- entry point og the program
+ *     * takes no argument*
+ *
+ *         * Description: determing if int is positve or negative usinf ifelse
+ *
+ *           * Return: Program always return 0(success)
+ *
+ *             */
+
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+			n = rand() - RAND_MAX / 2;
+			if (n > 0)							{
+
+			printf("%d is positive\n", n);
+											}
+			else if (n == 0)								{
+			printf("%d is zero\n", n);
+											}
+			else
+		{											printf("%d is negative\n", n);
+												}
+
+			return (0);
+
+}
