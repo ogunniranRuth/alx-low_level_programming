ed954ddc7b6788d0f95e5c2dbc0c50e1417eeda4
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-13 07:35:15 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * * main entry
+ * * main - print _putchar
  * * Return: Always 0.
  */
 int main(void)
 {
 	char *sh = "_putchar";
-	
 	while (*sh)
 	{
 		putchar(*sh);
 		sh++;
 	}
-	
 	putchar('\n');
-	
 	return (0);
 }
