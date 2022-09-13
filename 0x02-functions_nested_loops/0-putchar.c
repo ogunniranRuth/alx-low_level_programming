ab1801e9a1911223ce7e01ec0c64f49d98c985a0
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-13 04:27:35 -0700

    commit 0 task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * * main entry
+ * * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	c = getchar();
+
+	while (c != EOF) {
+
+		    putchar(c);
+		    c = getchar();
+		    putchar('\n');
+
+		    return (0);
+	}
+
+}
