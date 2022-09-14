bc25ffef00d0b41ee5597bc2d9ca6f6764c6c3d4
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-14 08:39:37 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			for (mult = 1; mult <=9; ++mult)
 			{
 				_putchar(',');
-				_putchar('');
+				_putchar(' ');
 
 				prod = num * mult;
 				/*
@@ -27,7 +27,7 @@ void times_table(void)
 				 * place the first digit if its two numbers
 				 */
 				if (prod <= 9)
-					_putchar('');
+					_putchar(' ');
 				else
 					_putchar((prod / 10) + 48); /* get the first digit*/
 
@@ -36,3 +36,4 @@ void times_table(void)
 			_putchar('\n');
 		}
 	}
+}
