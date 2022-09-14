af98eb0c9580afe028298635a3da23be3f2853e4
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-14 14:39:11 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
 * print_to_98 - print n to 98 counts
 * separated by comma, followed
-* by space and number should be 
+*by space and number should be 
 * printed in order
 * @n: input
 */
@@ -13,10 +13,11 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-		for (count = n; count < 98; --count)
+		for (count = n; count > 98; --count)
 			printf("%d, ", count);
 	else
 		for (count = n; count < 98; ++count)
-			printf("%d, ", count);
+			printf("%d, ", count)et number
+				
 	printf("98\n");
 }
