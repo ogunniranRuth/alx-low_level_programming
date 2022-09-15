07388167ce973ca184bfa67c7de90c17f0d34921
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-15 11:50:11 -0700

    task main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,5 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
