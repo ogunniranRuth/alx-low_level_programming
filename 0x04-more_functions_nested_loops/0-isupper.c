2b8c387bb8d017836a783364004feb53624733a4
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-15 04:46:01 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character entered
+ * Return: returns 1 if it is uppercase and 0 if it is not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (i);
+	}
+	else
+		return (0);
+}
+
