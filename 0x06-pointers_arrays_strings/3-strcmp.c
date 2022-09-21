b020ae6a9f2a889cd5c7580afbb4921542e0f6f0
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-21 07:54:18 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> function that compare two strings
+* @s1: parameter for string one
+* @s2: parameter for string two
+* Return:
+* 	returns zere if s1 == s2
+* 	returns negative value if s1 < s2
+* 	returns positive value if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0'
+				break;
+				else if (s1[i] == '\0')
+				{
+				diff = s2[i];
+				break;
+				}
+				else if (s2[i] == '\0')
+				{
+				diff = s1[i];
+				break;
+				}
+				else if (s1[i] != s2[i]
+					{
+					diff = s1[i] - s2[i];
+					break;
+					}
+					else
+					i++;
+	}
+					return (diff);
+}
