3b4dce3c8422341f0794737490c5b7327b3409db
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-20 19:28:03 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#inclu "main.h"
 /**
-*_strlen - Returns the length of a string.
-* @s: input string.
-* Return: length of a string.
+* swap_int - Swaps the values of two integers.
+* @a: first interger.
+* @b: second interger.
+* Return: no return.
+*
 */
 
-int _strlen(char *s)
+void swap_int(int *a, int *b)
 {
-	int count = 0;
-
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
