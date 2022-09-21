685b131dee9783d47d4bb1535e780c9698110deb
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-20 19:08:47 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-/*** puts_half - prints a string
+/** puts_half - prints a string
 * @str: string to print
 * Return: nothing
 */
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 	int i, len = _strlen(str);
 
-	for (i = ((len -1) / 2) + 1; i < len; i++)
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
 		putchar(*(str + 1));
 	putchar(10);
 }
