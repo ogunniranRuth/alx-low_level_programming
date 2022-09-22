ebdad383df75edb1ef0869c61d6bb1f429d62d94
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-22 03:42:41 -0700

    task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,7 +12,7 @@ void print_line(char *c, int s, int l)
 {
 	int j, k;
 
-	for (j =0; j <= 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
 			printf("%02x", c[l * 10 + j]);
@@ -23,8 +23,8 @@ void print_line(char *c, int s, int l)
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[i * 10 + k] > 31 && c[i * 10 + k] < 127)
-			putchar(c[i * 10 + k]);
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
 	}
