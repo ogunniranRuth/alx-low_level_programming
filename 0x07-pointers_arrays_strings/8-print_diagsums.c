92caabea58c714993a6a0152194d40f7d13aed01
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-26 15:09:53 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - the sum pf the two diagonals of a square matrix of intergers
+* @a: 2d array of chars
+* @size: number of matrix
+* Reurn: vid
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+	while (i < size)
+	{
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++;
+		}
+		printf("%i, %i\n", sum1, sum2);
+		}
+
