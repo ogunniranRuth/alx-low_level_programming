e77d3d64ccbbb604b73b5a27bd288edb9fdcc775
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-26 14:16:52 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include" main.h"
+/**
+* _strpbrk - matches any character speicified
+* @s: this is the C string to be scanned.
+* @accet: character in strl that matchs of on the character
+* Return: string s that matched any character specified.
+*/
+
+char *_strpbrk (char *s, char *accept)
+{
+	int j;
+
+	while (*s != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+
+			j++;
+		}
+		s++;
+	}
+	return (0);
+}
