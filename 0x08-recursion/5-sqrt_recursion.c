126fa5b6a8847de98010f08d9d7bc4b027e553f8
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-29 03:09:15 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,6 +18,11 @@ int isSqrt(int n, int i)
 	}
 	return (isSqrt(n, i + 1));
 }
+/**
+* _sqrt_recursion - returns natural root of +ve int
+* @n: input number
+* Return: root of n otherwise -1 if false
+*/
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
