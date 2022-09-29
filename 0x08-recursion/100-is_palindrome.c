f00571f602da3e65bb57b4fcef611de7f0637b71
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-29 04:36:12 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,47 +7,42 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s)
+	if (!*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+* p1 - check if string is palindrome
+* @s: pointer to string
+* @l: position
+* Return: boolean
+*/
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + 1))
+	{
+		return (p1(s + 1, l - 2));
 	}
 	return (0);
 }
 
 /**
-* niPalindrome - check if string is palindrome
-* @s: pointer to string
-* @fi: first index
-* @li: last index
-* Return: 0 if not, 1 if true
-*/
-
-int niPalindrome(char *s, int fi, int li)
-{
-	if (fi < li && s[fi] == s[li])
-	{
-		niPalindrome(s, fi + 1, li - 1);
-	}
-	if (s[fi] != s[li])
-	{
-		return (0);
-	}
-	return (1);
-}
-
-/**
 * is_palindrome - checks if string is palindrome or not
 * @s: pointer to string
-* Return: 1 if palindrome otherwise return 0
+* Return: recursion
 */
 
 int is_palindrome(char *s)
 {
-	int fi, li;
+	int len = _strlen_recursion(s);
 
-	fi = 0;
-	li = _strlen_recursion(s) - 1;
-
-	return (niPalindrome(s, fi, li));
+	return (p1(s, len - 1));
 }
-
