621020fc10861e85cee8d7c106480f87a2cd9cff
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-09-29 02:24:55 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
- *
- * * _puts_recursion - prints a string
- *
- * * followed by a new line
- *
- * @s: string to be printed
+* _puts_recursion - prints a string
+* followed by a new line
+* @s: string to be printed
 */
 
-void _put_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
