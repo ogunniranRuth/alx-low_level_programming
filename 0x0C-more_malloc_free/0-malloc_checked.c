2df842fde5749343d034df69673840786aaa65e9
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-10-06 10:16:54 -0700

    task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,45 @@
 #include "main.h"
 /**
-*malloc_checked - array for prints a string
-*@b: number of memory
-*Return: void
+* string_nconcat - concatenates two strings.
+* @s1: first string
+* @s2: second string
+* @n: index
+* Return: char pointer
 */
 
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *p;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-	p = malloc(b);
-	if (p == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		S2 = "";
+
+	while (s1[size1] != '\0')
 	{
-		exit(98);
+		size1++;
 	}
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	if (n > size2)
+		n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (0);
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
 	return (p);
 }
+
