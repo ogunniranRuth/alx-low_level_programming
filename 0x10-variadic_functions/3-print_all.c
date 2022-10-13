8beca871c21e5c9c426f93c4b0fd68f097aefe1e
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-10-13 05:01:48 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,10 +35,10 @@ void print_all(const char * const format, ...)
 						break;
 						case 's':
 						str = va_arg(valist, char *), k = 1;
-						if ( !str)
+						if (!str)
 						{
-						printf("(nil)");
-						break;
+							printf("(nil)");
+							break;
 						}
 						printf("%s", str);
 						break;
