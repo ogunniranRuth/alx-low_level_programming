6cddb8818c17e3ad48885f12474069edfa2633dd
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-10-13 05:20:39 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,16 +35,15 @@ void print_all(const char * const format, ...)
 						printf("%f", va_arg(valist, double)), k = 1;
 						break;
 						case 's':
-							str = va_arg(valist, char *), k = 1;
-							if (!str)
-							{
-								printf("(nil)");
-								break;
-							}
+						str = va_arg(valist, char *), k = 1;
+						if (!str)
+						{
+							printf("(nil)");
+							break;
+						}
 						printf("%s", str);
 						break;
-		}
-						i++;
+		} i++;
 	}
 	printf("\n"), va_end(valist);
 }
