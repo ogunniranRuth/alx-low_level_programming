9f86ef6cf1b2b9e06c1315bb95a9fb8bf899da44
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-11-01 08:01:28 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from = -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -35,14 +35,14 @@ int main(int argc, char *argv[])
 	ssize_t nchars, nwr;
 	char buf[1024];
 
-	if (argc !=3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	fle_to = open(argv[2], O_CREAT | 0_WRONLY | O_TRUNC | O__APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit (100);
+		exit(100);
 	}
 	return (0);
 }
